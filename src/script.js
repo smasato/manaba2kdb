8a0d33c1a6a13b02463b22b67b3f5eca9ddac845
Masato Sugiyama <public@smasato.net>
2021-10-08 19:40:29 +0900

    eslint fix

--- src/script.js
+++ src/script.js
@@ -1,37 +1,31 @@
-const getCourseCodeElement = () => {
-    return document.getElementsByClassName('coursecode')[0]
-}
+const getCourseCodeElement = () => document.getElementsByClassName('coursecode')[0];
+const isValidCourseCode = (courseCode) => /[A-Z0-9]{7,}/.test(courseCode);
 
 const getCourseCode = (element) => {
-    let courseCode
-    if (element) {
-        courseCode = element.innerText
-    }
+  let courseCode;
+  if (element) {
+    courseCode = element.innerText;
+  }
 
-    if (!isValidCourseCode(courseCode)) {
-        return null
-    }
-    return courseCode
-}
+  if (!isValidCourseCode(courseCode)) {
+    return null;
+  }
+  return courseCode;
+};
 
-const getYear = () => {
-    return document.getElementsByClassName('coursedata-info')[0].innerText.split(' ')[0]
-}
-
-const isValidCourseCode = (courseCode) => {
-    return /[A-Z0-9]{7,}/.test(courseCode)
-}
+const getYear = () => document.getElementsByClassName('coursedata-info')[0].innerText.split(' ')[0];
 
 const replaceCourseCode = (element, courseCode, year) => {
-    const url = `https://kdb.tsukuba.ac.jp/syllabi/${year}/${courseCode}/jpn`
-    aTag = `<a href="${url}" target="_blank">` + courseCode + "</a>"
-    element.innerHTML = aTag
-}
+  const url = `https://kdb.tsukuba.ac.jp/syllabi/${year}/${courseCode}/jpn`;
+  const aTag = `<a href="${url}" target="_blank">${courseCode}</a>`;
+  // eslint-disable-next-line no-param-reassign
+  element.innerHTML = aTag;
+};
 
-const element = getCourseCodeElement()
-const courseCode = getCourseCode(element)
-const year = getYear()
+const element = getCourseCodeElement();
+const courseCode = getCourseCode(element);
+const year = getYear();
 
 if (courseCode !== null) {
-    replaceCourseCode(element, courseCode, year)
+  replaceCourseCode(element, courseCode, year);
 }
