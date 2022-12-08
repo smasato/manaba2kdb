f219ea4974cfe12dfeded62518a9299a9bb3be59
Masato Sugiyama <public@smasato.net>
2022-12-08 14:03:59 +0900

    v1.1.0 (#7)
    * prepare for english
    
    * fix indent
    
    * Bump to 1.1.0

--- src/script.js
+++ src/script.js
@@ -1,12 +1,19 @@
-const syllabusURL = (courseCode, year) =>
-  `https://kdb.tsukuba.ac.jp/syllabi/${year}/${courseCode}/jpn`
+const getLanguage = () => {
+  const language = document.querySelector("html").getAttribute("lang")
+  if (language === "ja") return "jpn"
+  if (language === "en") return "eng"
+  return "jpn"
+}
+
+const syllabusURL = (courseCode, year, lang) =>
+  `https://kdb.tsukuba.ac.jp/syllabi/${year}/${courseCode}/${lang}`
 
 const getCourseCode = (element) => element.innerText
 const getYear = (element) => element.innerText.split(" ").shift()
 
-const syllabiAnchorElement = (courseCode, year) => {
+const syllabiAnchorElement = (courseCode, year, lang) => {
   const element = document.createElement("a")
-  element.setAttribute("href", syllabusURL(courseCode, year))
+  element.setAttribute("href", syllabusURL(courseCode, year, lang))
   element.setAttribute("target", "_blank")
   element.setAttribute("rel", "external")
   element.innerText = courseCode
@@ -17,20 +24,21 @@ const syllabiAnchorElement = (courseCode, year) => {
 const isValidCourseCode = (courseCode) =>
   /[A-Z0-9]{7,}/.test(courseCode)
 
-const replaceCourseCode = (element, courseCode, year) =>
-  (element.innerHTML = syllabiAnchorElement(courseCode, year).outerHTML)
+const replaceCourseCode = (element, courseCode, year, lang) =>
+  (element.innerHTML = syllabiAnchorElement(courseCode, year, lang).outerHTML)
 
 const main = () => {
-    const courseCodeElement = document.querySelector(".coursecode")
-    const courseDataInfoElement =
-      document.querySelector(".coursedata-info")
-    if (!courseCodeElement || !courseDataInfoElement) return
+  const courseCodeElement = document.querySelector(".coursecode")
+  const courseDataInfoElement =
+    document.querySelector(".coursedata-info")
+  if (!courseCodeElement || !courseDataInfoElement) return
 
+  const lang = "jpn" // TODO: getLanguage()
   const courseCode = getCourseCode(courseCodeElement)
   const year = getYear(courseDataInfoElement)
   if (!isValidCourseCode(courseCode) || !year) return
 
-  replaceCourseCode(courseCodeElement, courseCode, year)
+  replaceCourseCode(courseCodeElement, courseCode, year, lang)
 }
 
 main()
